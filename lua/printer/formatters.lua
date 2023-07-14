b84570f7b86247bf6643a94bd2a41f5d5bad73eb
rareitems <rareitems1@protonmail.com>
2023-07-15 01:18:51 +0200

    format

--- lua/printer/formatters.lua
+++ lua/printer/formatters.lua
@@ -1,61 +1,69 @@
 local formatters = {
-  lua = function(text_inside, text_var)
-    return string.format("print([[%s = ]] .. %s)", text_inside, text_var)
-    -- for nvim stuff
-    -- return string.format('print([[%s: ]] .. vim.inspect(%s))', text_inside, text_var)
-  end,
+    lua = function(text_inside, text_var)
+        return string.format("print([[%s = ]] .. %s)", text_inside, text_var)
+        -- for nvim stuff
+        -- return string.format('print([[%s: ]] .. vim.inspect(%s))', text_inside, text_var)
+    end,
 
-  python = function(text_inside, text_var)
-    return string.format('print("%s =", %s)', text_inside, text_var)
-  end,
+    python = function(text_inside, text_var)
+        return string.format('print("%s =", %s)', text_inside, text_var)
+    end,
 
-  javascript = function(text_inside, text_var)
-    return string.format('console.log("%s = ", %s)', text_inside, text_var)
-  end,
+    javascript = function(text_inside, text_var)
+        return string.format('console.log("%s = ", %s)', text_inside, text_var)
+    end,
 
-  typescript = function(text_inside, text_var)
-    return string.format('console.log("%s = ", %s)', text_inside, text_var)
-  end,
+    typescript = function(text_inside, text_var)
+        return string.format('console.log("%s = ", %s)', text_inside, text_var)
+    end,
 
-  go = function(text_inside, text_var)
-    return string.format('fmt.Println("%s = ", %s)', text_inside, text_var)
-  end,
+    go = function(text_inside, text_var)
+        return string.format('fmt.Println("%s = ", %s)', text_inside, text_var)
+    end,
 
-  vim = function(text_inside, text_var)
-    return string.format('echo "%s = ".%s', text_inside, text_var)
-  end,
+    vim = function(text_inside, text_var)
+        return string.format('echo "%s = ".%s', text_inside, text_var)
+    end,
 
-  rust = function(text_inside, text_var)
-    return string.format([[println!("%s = {:#?}", %s);]], text_inside, text_var)
-  end,
+    rust = function(text_inside, text_var)
+        return string.format([[println!("%s = {:#?}", %s);]], text_inside, text_var)
+    end,
 
-  zsh = function(text_inside, text_var)
-    return string.format('echo "%s = $%s"', text_inside, text_var)
-  end,
+    zsh = function(text_inside, text_var)
+        return string.format('echo "%s = $%s"', text_inside, text_var)
+    end,
 
-  bash = function(text_inside, text_var)
-    return string.format('echo "%s = $%s"', text_inside, text_var)
-  end,
+    bash = function(text_inside, text_var)
+        return string.format('echo "%s = $%s"', text_inside, text_var)
+    end,
 
-  sh = function(text_inside, text_var)
-    return string.format('echo "%s = $%s"', text_inside, text_var)
-  end,
+    sh = function(text_inside, text_var)
+        return string.format('echo "%s = $%s"', text_inside, text_var)
+    end,
 
-  java = function(text_inside, text_var)
-    return string.format('System.out.println("%s = " + %s);', text_inside, text_var)
-  end,
+    java = function(text_inside, text_var)
+        return string.format('System.out.println("%s = " + %s);', text_inside, text_var)
+    end,
 
-  cs = function(text_inside, text_var)
-    return string.format('System.Console.WriteLine("%s = " + %s);', text_inside, text_var)
-  end,
+    cs = function(text_inside, text_var)
+        return string.format(
+            'System.Console.WriteLine("%s = " + %s);',
+            text_inside,
+            text_var
+        )
+    end,
 
-  cpp = function(text_inside, text_var)
-    return string.format('std::cout << "%s = " << %s << std::endl;', text_inside, text_var)
-  end,
+    cpp = function(text_inside, text_var)
+        return string.format(
+            'std::cout << "%s = " << %s << std::endl;',
+            text_inside,
+            text_var
+        )
+    end,
 
-  ruby = function(inside, variable)
-    return string.format('pp "%s = ", %s', inside, variable)
-  end,
+    ruby = function(inside, variable)
+        return string.format('pp "%s = ", %s', inside, variable)
+    end,
 }
 
 return formatters
--- lua/printer/init.lua
+++ lua/printer/init.lua
@@ -21,7 +21,7 @@ local UsersFormatters = {}
 local Behavior = nil
 
 local notify = function(text)
-  vim.notify("PRINTER: " .. text)
+    vim.notify("PRINTER: " .. text)
 end
 
 -- local notify_error = function(text)
@@ -30,126 +30,164 @@ end
 
 -- Get range of a textobject
 local function get_textobject_range()
-  local marks = { "[", "]" }
-  local start, endd = vim.api.nvim_buf_get_mark(0, marks[1]), vim.api.nvim_buf_get_mark(0, marks[2])
-  return { srow = start[1], scol = start[2], erow = endd[1], ecol = endd[2] }
+    local marks = { "[", "]" }
+    local start, endd =
+        vim.api.nvim_buf_get_mark(0, marks[1]), vim.api.nvim_buf_get_mark(0, marks[2])
+    return { srow = start[1], scol = start[2], erow = endd[1], ecol = endd[2] }
 end
 
 -- Get text from a range of a textobject
 local function get_text_from_textobject()
-  local range = get_textobject_range()
-  if range.srow == range.erow then
-    -- rows (lines) are 1 based indexed but have to be 0-based and inclusive so substracting 1 from both start and end
-    -- columns are 0 based indexed, have to be exclusive, so adding 1 to the end
-    return vim.api.nvim_buf_get_text(0, range.srow - 1, range.scol, range.erow - 1, range.ecol + 1, {})
-  else
-    notify("printer.nvim doesn't support multiple lines textobjects")
-    return nil
-  end
+    local range = get_textobject_range()
+    if range.srow == range.erow then
+        -- rows (lines) are 1 based indexed but have to be 0-based and inclusive so substracting 1 from both start and end
+        -- columns are 0 based indexed, have to be exclusive, so adding 1 to the end
+        return vim.api.nvim_buf_get_text(
+            0,
+            range.srow - 1,
+            range.scol,
+            range.erow - 1,
+            range.ecol + 1,
+            {}
+        )
+    else
+        notify("printer.nvim doesn't support multiple lines textobjects")
+        return nil
+    end
 end
 
 -- Get range of a visual selection
 local function get_visual_range()
-  local marks = { "<", ">" }
-  local start, endd = vim.api.nvim_buf_get_mark(0, marks[1]), vim.api.nvim_buf_get_mark(0, marks[2])
-  return { srow = start[1], scol = start[2], erow = endd[1], ecol = endd[2] }
+    local marks = { "<", ">" }
+    local start, endd =
+        vim.api.nvim_buf_get_mark(0, marks[1]), vim.api.nvim_buf_get_mark(0, marks[2])
+    return { srow = start[1], scol = start[2], erow = endd[1], ecol = endd[2] }
 end
 
 -- Get text from a range of a visual selection
 local function get_text_from_visualrange()
-  local range = get_visual_range()
-  if range.srow == range.erow then
-    -- rows (lines) are 1 based indexed but have to be 0-based and inclusive so substracting 1 from both start and end
-    -- columns are 0 based indexed, have to be exclusive, so adding 1 to the end
-    return vim.api.nvim_buf_get_text(0, range.srow - 1, range.scol, range.erow - 1, range.ecol + 1, {})
-  else
-    notify("printer.nvim doesn't support multiple lines ranges")
-    return nil
-  end
+    local range = get_visual_range()
+    if range.srow == range.erow then
+        -- rows (lines) are 1 based indexed but have to be 0-based and inclusive so substracting 1 from both start and end
+        -- columns are 0 based indexed, have to be exclusive, so adding 1 to the end
+        return vim.api.nvim_buf_get_text(
+            0,
+            range.srow - 1,
+            range.scol,
+            range.erow - 1,
+            range.ecol + 1,
+            {}
+        )
+    else
+        notify("printer.nvim doesn't support multiple lines ranges")
+        return nil
+    end
 end
 
 local Printer = {}
 
 AddToInside = function(text)
-  return string.format("[%s:%s] %s", vim.fn.expand("%"), vim.fn.line("."), text)
+    return string.format("[%s:%s] %s", vim.fn.expand("%"), vim.fn.line("."), text)
 end
 
 local function input(text)
-  local filetype = vim.bo.filetype
-  local printer = vim.b["printer"] or vim.g.printer[filetype] or UsersFormatters[filetype] or require("printer.formatters")[filetype]
+    local filetype = vim.bo.filetype
+    local printer = vim.b["printer"]
+        or vim.g.printer[filetype]
+        or UsersFormatters[filetype]
+        or require("printer.formatters")[filetype]
 
-  if printer == nil then
-    notify("no formatter defined for " .. filetype .. " filetype. See ':help Printer.setting_custom_formatters' on how to add formatter for this filetype.")
-    return
-  end
-
-  if text ~= nil then
-    local text_to_insert
-
-    if AddToInside then
-      text_to_insert = printer(AddToInside(text), text)
-    else
-      text_to_insert = printer(text, text)
+    if printer == nil then
+        notify(
+            "no formatter defined for "
+                .. filetype
+                .. " filetype. See ':help Printer.setting_custom_formatters' on how to add formatter for this filetype."
+        )
+        return
     end
 
-    if Behavior == "insert_below" then
-      vim.fn.execute("normal! o" .. text_to_insert)
-    elseif Behavior == "yank" then
-      vim.fn.setreg('"', text_to_insert)
+    if text ~= nil then
+        local text_to_insert
+
+        if AddToInside then
+            text_to_insert = printer(AddToInside(text), text)
+        else
+            text_to_insert = printer(text, text)
+        end
+
+        if Behavior == "insert_below" then
+            vim.fn.execute("normal! o" .. text_to_insert)
+        elseif Behavior == "yank" then
+            vim.fn.setreg('"', text_to_insert)
+        end
     end
-  end
 end
 
 ---@private
 Printer._normal_print = function()
-  local text = get_text_from_textobject()[1]
-  if text then
-    input(text)
-  end
+    local text = get_text_from_textobject()[1]
+    if text then
+        input(text)
+    end
 end
 
 local function operator_normal()
-  vim.cmd([[set operatorfunc=v:lua.require'printer'._normal_print]])
-  return "g@"
+    vim.cmd([[set operatorfunc=v:lua.require'printer'._normal_print]])
+    return "g@"
 end
 
 ---@private
 Printer._visual_print = function()
-  local text = get_text_from_visualrange()[1]
-  if text then
-    input(text)
-  end
+    local text = get_text_from_visualrange()[1]
+    if text then
+        input(text)
+    end
 end
 
 local function operator_visual()
-  vim.cmd([[set operatorfunc=v:lua.require'printer'._visual_print]])
-  return "g@"
+    vim.cmd([[set operatorfunc=v:lua.require'printer'._visual_print]])
+    return "g@"
 end
 
 --- Used for setting initial configuration see |printer.configuration|
 Printer.setup = function(cfg_user)
-  cfg_user = cfg_user or {}
+    cfg_user = cfg_user or {}
 
-  if cfg_user.keymap then
-    vim.keymap.set("n", cfg_user.keymap, operator_normal, { expr = true, desc = "Operator keymap for printer.nvim" })
-    vim.keymap.set("v", cfg_user.keymap, operator_visual, { expr = true, desc = "Operator keymap for printer.nvim" })
-  else
-    notify("Printer config was called without a keymap")
-  end
-
-  vim.keymap.set("n", "<Plug>(printer_print)", operator_normal, { expr = true, desc = "Get text out of textobject formatted for debug printing" })
-
-  if cfg_user.add_to_inside then
-    if type(cfg_user.add_to_inside) == "function" then
-      AddToInside = cfg_user.add_to_inside
+    if cfg_user.keymap then
+        vim.keymap.set(
+            "n",
+            cfg_user.keymap,
+            operator_normal,
+            { expr = true, desc = "Operator keymap for printer.nvim" }
+        )
+        vim.keymap.set(
+            "v",
+            cfg_user.keymap,
+            operator_visual,
+            { expr = true, desc = "Operator keymap for printer.nvim" }
+        )
     else
-      notify("add_to_inside field is not a function")
+        notify("Printer config was called without a keymap")
     end
-  end
 
-  UsersFormatters = cfg_user.formatters or {}
-  Behavior = cfg_user.behavior or "insert_below"
-  vim.g.printer = {}
+    vim.keymap.set(
+        "n",
+        "<Plug>(printer_print)",
+        operator_normal,
+        { expr = true, desc = "Get text out of textobject formatted for debug printing" }
+    )
+
+    if cfg_user.add_to_inside then
+        if type(cfg_user.add_to_inside) == "function" then
+            AddToInside = cfg_user.add_to_inside
+        else
+            notify("add_to_inside field is not a function")
+        end
+    end
+
+    UsersFormatters = cfg_user.formatters or {}
+    Behavior = cfg_user.behavior or "insert_below"
+    vim.g.printer = {}
 end
 
 return Printer
